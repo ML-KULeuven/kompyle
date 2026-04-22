af688897c897a08f6c199f68f25544fb0fb4a259
IbrahimElk <ibrahim_elkaddouri@outlook.com>
2026-04-22 10:12:44 +0200

    build: set macOS deployment target for Boost build
    Signed-off-by: IbrahimElk <ibrahim_elkaddouri@outlook.com>

--- scripts/deps/01_boost.sh
+++ scripts/deps/01_boost.sh
@@ -8,7 +8,14 @@ wget -q https://archives.boost.io/release/1.90.0/source/boost_1_90_0.tar.gz
 tar xf boost_1_90_0.tar.gz
 cd boost_1_90_0
 ./bootstrap.sh --with-libraries=program_options
-$SUDO ./b2 -j$NPROC install
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    $SUDO ./b2 -j$NPROC install \
+      cxxflags="-mmacosx-version-min=11.0" \
+      linkflags="-mmacosx-version-min=11.0"
+else
+    $SUDO ./b2 -j$NPROC install
+fi
 cd ..
 # cleanup boost_1_90_0 boost_1_90_0.tar.gz
 ldconfig_if_linux
